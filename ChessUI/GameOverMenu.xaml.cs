e6b6b663f63cb5961155bccebb4e023305b37d60
daniel <danielserebro21@gmail.com>
2024-11-22 16:56:55 +0200

    End of part 11

---
+++ ChessUI/GameOverMenu.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ChessLogic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for GameOverMenu.xaml
+    /// </summary>
+    public partial class GameOverMenu : UserControl
+    {
+        public event Action<Option> OptionSelected;
+
+        public GameOverMenu(GameState gameState)
+        {
+            InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+        }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "WHITE WINS!",
+                Player.Black => "BLACK WINS!",
+                _ => "ITS A DRAW!"
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "WHITE",
+                Player.Black => "BLACK",
+                _ => ""
+            };
+        }
+
+
+        private static string GetReasonText(EndReason reason, Player player)
+        {
+            return reason switch
+            {
+                EndReason.Stalmate => $"STALEMATE - {PlayerString(player)} CANT MOVE",
+                EndReason.Checkmate => $"CHECKMATE - {PlayerString(player)} CANT MOVE",
+                EndReason.FiftyMoveRule => "FIFTY-MOVE RULE",
+                EndReason.InsufficientMaterial => "INSUFFICIENT MATERIAL",
+                EndReason.ThreeFoldRepetition => "THREEFOLD REPETITION",
+                _ => ""
+            };
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            OptionSelected?.Invoke(Option.Restart);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            OptionSelected?.Invoke(Option.Exit);
+        }
+    }
+}
